7d05a7daff8fd773dde0f716702d82f4a2b21430
James Bennion-Pedley <james@bojit.org>
2025-09-21 20:43:43 +0100

    Add local overlay for alternative RAM configuration

--- software/app/src/main.c
+++ software/app/src/main.c
@@ -23,8 +23,8 @@ LOG_MODULE_REGISTER(main, CONFIG_LOG_DEFAULT_LEVEL);
 
 static const int32_t sleep_time_ms = 500;
 
-K_THREAD_STACK_DEFINE(m_net_mgr_stack, 2048);
-struct k_thread m_net_mgr_thread;
+// K_THREAD_STACK_DEFINE(m_net_mgr_stack, 2048);
+// struct k_thread m_net_mgr_thread;
 
 /*------------------------------ Private Functions ---------------------------*/
 
@@ -34,19 +34,20 @@ struct k_thread m_net_mgr_thread;
 
 int main(void)
 {
-	net_mgr_init();
+	// net_mgr_init();
 
-	k_thread_create(&m_net_mgr_thread, m_net_mgr_stack, K_THREAD_STACK_SIZEOF(m_net_mgr_stack),
-			net_mgr_thread, NULL, NULL, NULL, 5, 0, K_NO_WAIT);
+	// k_thread_create(&m_net_mgr_thread, m_net_mgr_stack,
+	// K_THREAD_STACK_SIZEOF(m_net_mgr_stack), 		net_mgr_thread, NULL, NULL, NULL, 5,
+	// 0, K_NO_WAIT);
 
-	// int i = 0;
+	int i = 0;
 
 	while (1) {
-		// i++;
-		// LOG_INF("Hello World! %u", k_uptime_get_32());
-		// if (i % 10 == 0) {
-		// 	LOG_WRN("Random Panic!");
-		// }
+		i++;
+		LOG_INF("Hello World! %u", k_uptime_get_32());
+		if (i % 10 == 0) {
+			LOG_WRN("Random Panic!");
+		}
 		k_msleep(sleep_time_ms);
 	}
 }
